e3d7f0afb763e506e439f0ec065b9fb2baca4d0f
Chel <chel.doyle@mscoding.org>
2024-08-02 11:18:17 -0500

    up

---
+++ main.js
@@ -0,0 +1,28 @@
+let fruits = ["apple", "banana", "cherry"];
+console.log(fruits[1]);
+
+let numbers = [1, 2, 3, 4, 5];
+let sum = numbers[0] + numbers[2];
+console.log(sum);
+
+let languages = ["JavaScript", "Python", "Java"];
+languages[1] = "Ruby";
+console.log(languages);
+
+let age = 20;
+if (age >= 18) {
+    console.log("Eligible to vote");
+} else {
+    console.log("Not eligible to vote");
+};
+
+let cities = ["New York", "Los Angeles", "Chicago"];
+let lastCity = cities[cities.length - 1];
+console.log(lastCity);
+
+let x = 7;
+if (x > 5 && x < 10) {
+    console.log("In range");
+} else {
+    console.log("Out of range");
+};
